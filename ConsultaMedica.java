f658f23b5dc0fee12e7c66a5d1922c436381e5ba
DmV554 <daniel3318bl@gmail.com>
2023-08-26 18:33:51 -0400

    Modularización inicial

---
+++ ConsultaMedica.java
@@ -0,0 +1,10 @@
+public class ConsultaMedica {
+    private String descripcion;
+    private String hora;
+    private String fecha;
+    private String medico;
+    private String motivoVisita;
+
+    public ConsultaMedica() {
+    }
+}
---
+++ Sistema.java
@@ -0,0 +1,25 @@
+public class Sistema {
+    Pacientes[] pacientes = new Pacientes[100];
+
+    public Sistema() {
+    }
+
+    public void agregarPaciente() {
+        System.out.println("AÃºn no implementado!");
+    }
+
+    public void eliminarPaciente() {
+    }
+
+    public void mostrarPacientes() {
+    }
+
+    public void agregarCita() {
+    }
+
+    public void eliminarCita() {
+    }
+
+    public void listarConsultasPorPaciente() {
+    }
+}
