682ff12f57247847001c6217dc1b86bf441d2686
DmV554 <daniel3318bl@gmail.com>
2023-08-28 10:48:21 -0400

    Clase ConsultaMedica con sus setters y getters

--- ConsultaMedica.java
+++ ConsultaMedica.java
@@ -7,4 +7,44 @@ public class ConsultaMedica {
 
     public ConsultaMedica() {
     }
+
+    public void setDescripcion(String var1) {
+        this.descripcion = var1;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setHora(String var1) {
+        this.hora = var1;
+    }
+
+    public String getHora() {
+        return this.hora;
+    }
+
+    public void setFecha(String var1) {
+        this.fecha = var1;
+    }
+
+    public String getFecha() {
+        return this.fecha;
+    }
+
+    public void setMedico(String var1) {
+        this.medico = var1;
+    }
+
+    public String getMedico() {
+        return this.medico;
+    }
+
+    public void setMotivoVisita(String var1) {
+        this.motivoVisita = var1;
+    }
+
+    public String getMotivoVisita() {
+        return this.motivoVisita;
+    }
 }
